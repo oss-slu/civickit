cd17e1368c2b0f9cad4fd805d29e9a2d334d5946
Erin Kelley <erinkelley@Mac.lan>
2026-01-30 20:51:04 -0600

    created user authentication endpoint

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,21 @@
+// backend/src/controllers/auth.controller.ts
+
+import { Request, Response, NextFunction } from "express";
+import { AuthService } from "../services/auth.service";
+import { AuthRepository } from "../repositories/auth.repository";
+import { CreateAuthDTO } from "../types/auth.types";
+
+const authRepository = new AuthRepository();
+const authService = new AuthService(authRepository);
+
+export class AuthController {
+  async register(req: Request<{}, {}, CreateAuthDTO>, res: Response, next: NextFunction) {
+    try {
+      const { email, password, name } = req.body;
+      const user = await authService.registerUser({ email, password, name });
+      return res.status(201).json(user);
+    } catch (error: any) {
+      next(error);
+    }
+  }
+}
