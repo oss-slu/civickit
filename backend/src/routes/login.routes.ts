a38b608ac08675d990629f6ea59b2daf33f564bb
Leah B <leahb2613@gmail.com>
2026-02-02 11:17:00 -0600

    moved rate limiter to login.routes

--- backend/src/routes/login.routes.ts
+++ backend/src/routes/login.routes.ts
@@ -5,5 +5,13 @@ import { LoginController } from '../controllers/login.controller';
 const router = Router();
 const loginController = new LoginController();
 
+// Rate Limiter
+var RateLimit = require('express-rate-limit')
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 100, //max 100 requests per window
+})
+router.use(limiter)
+
 router.post('/', loginController.login);
 export default router;
