09d5434af9afb068b07ae09bbda1683ef1539a21
Leah B <leahb2613@gmail.com>
2026-02-12 18:40:48 -0600

    now gets user location, added details to issue card

--- mobile/src/screens/IssueListScreen.tsx
+++ mobile/src/screens/IssueListScreen.tsx
@@ -1,60 +1,173 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, RefreshControl, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import { QueryClient, useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
+import React from 'react';
+import IssueDetailScreen from './IssueDetailScreen';
+import * as Location from 'expo-location'
+// import { getDistance } from 'geolib';
 
 export default function IssueListScreen() {
+  const [refreshing, setRefreshing] = useState(false)
+  const [selectedIssue, setSelectedIssue] = useState()
+  const [isIssueSelected, setIsIssueSelected] = useState(false)
+  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false)
+  const [userLatitude, setUserLatitude] = useState<number>()
+  const [userLongitude, setUserLogitude] = useState<number>()
+  
+  //get user location
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, [])
+  
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+    if(!enabled) {
+      Alert.alert('Location not enabled', 
+        'Please enabled your Location', [
+        {text: 'Cancel'},
+        {text: 'OK'}
+      ])
+    } else {
+      setLocationServicesEnabled(enabled)
+    }
+  }
 
+  const getCurrentLocation = async () => {
+    //check permission
+    let {status} = await Location.requestForegroundPermissionsAsync()
+    if(status !== 'granted'){
+      Alert.alert('Permission denied', 
+        'Grant permission to use location services',[
+          {text: 'Cancel'},
+          {text: 'OK'}
+        ])
+      setLocationServicesEnabled(false)
+    } 
+
+    //get lat and long
+    const {coords} = await Location.getCurrentPositionAsync()
+
+    if(coords){
+      const {latitude, longitude} = coords;
+      setUserLatitude(latitude)
+      setUserLogitude(longitude)
+    }
+  }
+  
+  //fetch issues from database 
+    //runs even when location is denied to prevent hook errors
   const queryClient = new QueryClient()
-
   const { data, isLoading, error, refetch } = useQuery({
       queryKey: ['issues', 'nearby'],
       queryFn: async () => {
+          //using ipv4 address to test on expo go
+          //will need to be adjusted per individual computer
           const response = await fetch(
-          'http://localhost:3000/api/issues/nearby?lat=38.6270&lng=-90.1994&radius=5000'
+          'http://192.168.12.172:3000/api/issues/nearby?lat=' + 
+            userLatitude + '0&lng=' + userLongitude + '&radius=5000'
           );
+          console.log("fetch", response)
           if (!response.ok) throw new Error('Failed to fetch');
           return response.json();
       } 
   }, queryClient);
 
-  type IssueProps = {title: String};
-  const Issue = ({title}: IssueProps) => (
-    <View >
-      <Text style={styles.issueTitle}>{title}</Text>
-    </View>
+  //return error message for location permission denied
+  if(!locationServicesEnabled){
+    return (
+      <ScrollView contentContainerStyle={styles.container} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refetch}/>}>
+        <Text style={styles.title}>Location Permission Denied</Text>
+      </ScrollView>
+    )
+  }
+
+  //onPress behaviour
+  const onPress = (issue:any) => {
+    setSelectedIssue(issue)
+    setIsIssueSelected(true)
+  }
+
+  //Flatlist item setup
+  type IssueProps = {title: String,
+                      category: String,
+                      latitude: number,
+                      longitude: number,
+                      upvoteCount: number
+                    onPress: () => void};
+  const Issue = ({title, category,
+                  latitude, longitude,
+                   upvoteCount, onPress}: IssueProps) => (
+    <TouchableOpacity onPress={onPress}>
+      <View>
+        <Text style={styles.issueTitle}>{title}</Text>
+        <Text style={styles.issueInfo}>{category}</Text>
+        <Text style={styles.issueInfo}>{latitude}, {longitude}</Text>
+        <Text style={styles.issueInfo}>{upvoteCount}</Text>
+      </View>
+    </TouchableOpacity>
   )
   
+  //check if still loading
   if(!isLoading){
-    console.log(data, error)
+    //check if an error had been thrown
     if(error == undefined){
-      return (
-        <View style={styles.container}>
-          <Text style={styles.title}>Nearby Issues</Text>
-          <FlatList
-            data = {data.issues}
-            renderItem = {({item}) => <Issue title = {item.title}/>}
-            keyExtractor = {(item) => item.id}
-          />
-        </View>
-      );
+      //check if any data was returned
+      if(data.issues.length != 0){
+
+        if(isIssueSelected){
+          return (
+            <IssueDetailScreen issue = {selectedIssue}
+                                isIssueSelected = {isIssueSelected}
+                                setIsIssueSelected = {setIsIssueSelected}></IssueDetailScreen>
+          )
+        } else {
+          return (
+            <View style={styles.container}>
+              <Text style={styles.title}>Nearby Issues</Text>
+              <FlatList
+                data = {data.issues}
+                renderItem = {({item}) => <Issue title = {item.title}
+                                            category={item.category} 
+                                            latitude={item.latitude}
+                                            longitude={item.longitude}
+                                            upvoteCount={item.upvoteCount}
+                                            onPress={() => onPress(item)}/>}
+                keyExtractor = {(item) => item.id}
+                refreshControl= {<RefreshControl refreshing={refreshing} 
+                                        onRefresh={refetch} />}
+                extraData={selectedIssue}
+              />
+            </View>
+          );
+        }
+        
+      } else {
+        return (
+          <ScrollView contentContainerStyle={styles.container} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refetch}/>}>
+            <Text style={styles.title}>No issues nearby</Text>
+          </ScrollView>
+        )
+      }
+      
     }  else {
         return (
-        <View style={styles.container}>
-          <Text style={styles.title}>{String(error)}</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refetch}/>}>
+          <Text style={styles.title}>There was an error</Text>
+        </ScrollView>
       )
-    }    
+    }   
+
   } else {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Loading</Text>
+        <Text style={styles.title}>Loading...</Text>
       </View>
     )
   }
   
 }
-
 
 
 const styles = StyleSheet.create({
@@ -71,5 +184,8 @@ const styles = StyleSheet.create({
   },
   issueTitle: {
     fontSize: 20
+  },
+  issueInfo: {
+    fontSize: 15
   }
 });
