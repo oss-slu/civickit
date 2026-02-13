41c0efed0b7c24940655f0b9ffaad45585298edf
Erin Kelley <erinkelley@Mac.lan>
2026-02-13 09:56:36 -0600

    created issue card component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,15 +2,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
+import IssueCardTestScreen from './src/screens/IssueCardTestScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  // return (
+  //   <NavigationContainer>
+  //     <Stack.Navigator>
+  //       <Stack.Screen name="Home" component={HomeScreen} />
+  //     </Stack.Navigator>
+  //   </NavigationContainer>
+  // );
+
+  return <IssueCardTestScreen />;
 }
