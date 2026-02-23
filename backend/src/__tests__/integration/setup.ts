d5b17d0b3a2966f6202c4e150f633d922fe40cbe
bhuelsman <briana.huelsman@gmail.com>
2026-02-22 18:40:40 -0600

    implemented issue creation unit testing service layer

---
+++ backend/src/__tests__/integration/setup.ts
@@ -0,0 +1,48 @@
+// backend/src/__tests__/setup.ts
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.test' });
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { execSync } from 'child_process';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL not defined in .env.test")
+}
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+
+// Runs before all tests
+beforeAll(async () => {
+  // Set test database URL
+  //process.env.DATABASE_URL = process.env.TEST_DATABASE_URL || 'postgresql://user:password@localhost:5432/civickit_test';
+    
+  // Reset database and run migrations
+  execSync('npx prisma migrate reset --force', {
+    //env: { ...process.env, DATABASE_URL: process.env.DATABASE_URL },
+    env: {...process.env},
+  });
+});
+
+// Runs after all tests
+afterAll(async () => {
+  if (prisma) {
+    await prisma.$disconnect();
+  }
+});
+
+// Clear database between test suites
+afterEach(async () => {
+  const tables = ['User', 'Issue', 'Comment', 'Upvote', 'Event', 'EventRsvp'];
+  
+  for (const table of tables) {
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE "${table}" CASCADE;`);
+  }
+});
+
+export {prisma}; //exporting use for tests
