cd101cac075dc818da6dbf68b48cf357fce7a4a9
Leah B <leahb2613@gmail.com>
2026-02-27 08:17:34 -0600

    added feilds for issue creation and startup scripts

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -2,7 +2,6 @@
 
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import { LoginController } from '../controllers/login.controller';
 
 // Extend Express Request type
 declare global {
@@ -14,15 +13,15 @@ declare global {
 }
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  
+
   try {
     //get token from Authorization: Bearer header
     const token = String(req.headers.authorization?.substring(7))
-    
+
     //Verify Token
     const secret = String(process.env.JWT_SECRET)
-    const tokenResponse  = jwt.verify(token, secret) as JwtPayload
-    
+    const tokenResponse = jwt.verify(token, secret) as JwtPayload
+
     //attach userId to request
     req.userId = tokenResponse.userId
 
@@ -32,6 +31,6 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
   } catch (error) {
     throw error
   }
-  
-  
+
+
 };
