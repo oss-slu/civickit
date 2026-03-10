992f034d81d93374e7aac56721765ed56d7bb3b4
Leah B <leahb2613@gmail.com>
2026-03-09 21:25:53 -0500

    switched to regex matching

--- mobile/startWin.sh
+++ mobile/startWin.sh
@@ -4,16 +4,21 @@
 
 if [ $1 == "ip" ]; then
     IP_LINE=$(ipconfig | findstr IPv4 )
-    IP=${IP_LINE:39:40}
 
-    # echo ${IP_LINE:39:(${#IP_LINE}-39)}
+    if [[ "$IP_LINE" =~ [[:digit:]]{1,3}.[[:digit:]]{1,3}.[[:digit:]]{1,3}.[[:digit:]]{1,3} ]]; then
+        IP=${BASH_REMATCH[0]}
 
-    cd src/config
-    jq -n --arg IP $IP '{"domain":$IP}' > env.local.json
+        cd src/config
+        jq -n --arg IP $IP '{"domain":$IP}' > env.local.json
 
-    echo starting with ip address
-    cd ../..
-    npx expo start
+        echo starting with ip address
+        cd ../..
+        npx expo start
+    else 
+        echo no ipv4 found
+    fi
+
+    
 elif [ $1 == "localhost" ]; then
     LOCALHOST="localhost"
     cd src/config
