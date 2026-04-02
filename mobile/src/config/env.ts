f2439f3e6c218fecad1f8a2ccf9297f0568e1826
Briana Huelsman <149435983+bhuelsman@users.noreply.github.com>
2026-04-01 21:07:28 -0500

    Clean up env.ts and resolve merge conflicts
    Removed commented-out API URLs and resolved merge conflicts.

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -5,11 +5,7 @@ const ENV = {
     dev: {
         // apiUrl: 'http://localhost:3000/api',
         apiUrl: 'http://' + local.domain + ':3000/api' //use this when no network errors
-<<<<<<< 75-create-navigation-bar-component
         // apiUrl: 'https://quarters-elections-idea-restaurants.trycloudflare.com'+'/api' //cloudflare tunnel URL, should be replaced later
-=======
-        //apiUrl: 'https://consumer-evening-ends-britain.trycloudflare.com'+'/api' //cloudflare tunnel URL, should be replaced later
->>>>>>> main
     },
     prod: {
         apiUrl: 'https://civickit.loca.lt/api', //localtunnel URL, should be replaced later
