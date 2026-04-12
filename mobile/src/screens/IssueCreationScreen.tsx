dbdaecf14676521b86ce927f9c96684f77e93efb
Leah B <leahb2613@gmail.com>
2026-04-11 19:47:44 -0500

    deleted moved functions"

--- mobile/src/screens/IssueCreationScreen.tsx
+++ mobile/src/screens/IssueCreationScreen.tsx
@@ -64,46 +64,6 @@ export default function IssueCreationScreen() {
         })();
     }, []);
 
-    //handle images
-    const pickImage = async () => {
-        if (images.length < 5) {
-            const results = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: ['images'],
-                quality: 0.8,
-                allowsMultipleSelection: true,
-                selectionLimit: 5 - images.length
-            })
-            if (!results.canceled) {
-                //does not work on web, retruns unusable uri
-                const resultList = results.assets.map(r => r.uri)
-                setImages([...images, ...resultList]);
-            }
-        }
-
-    };
-
-    const openCamera = async () => {
-        console.log("Opening camera")
-        const { status } = await ImagePicker.requestCameraPermissionsAsync();
-
-        if (status !== 'granted') {
-            alert("Camera permission denied");
-            return;
-        }
-
-        if (images.length < 5) {
-            const result = await ImagePicker.launchCameraAsync({
-                mediaTypes: ['images'],
-                quality: 0.8,
-
-            });
-            //does not work on web, returns unusable uri
-            if (!result.canceled) {
-                setImages([...images, result.assets[0].uri]);
-            }
-        }
-    };
-
     const onImageDeletePressed = (image: any) => {
         setImages(
             images.filter(i => i != image)
