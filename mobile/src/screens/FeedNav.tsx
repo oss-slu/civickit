1f340402500e1bc6941438e9959b894fb12ebd1f
Leah B <leahb2613@gmail.com>
2026-04-26 22:34:12 -0500

    fix name and imports

--- mobile/src/screens/FeedNav.tsx
+++ mobile/src/screens/FeedNav.tsx
@@ -1,10 +1,10 @@
-// mobile/src/screens/LandingScreen.tsx
+// mobile/src/screens/FeedNav.tsx
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StackParams } from '../types/StackParams';
 import { colors, typography } from '../styles';
 import { ImagesContext, UserLocationContext, AddressContext, TitleContext, CategoryContext, DescriptionContext } from '../contexts/FormContexts';
 import FeedScreen from './FeedScreen';
-import LeaderBoard from './LeaderboardScreen';
+import LeaderBoardScreen from './LeaderboardScreen';
 import IssueDetailScreen from './IssueDetailScreen';
 import ErrorScreen from './ErrorScreen';
 
@@ -28,9 +28,9 @@ export default function FeedNav() {
                 options={{
                     headerShown: true
                 }} />
-            <Stack.Screen name="Leaderboard" component={LeaderBoard} />
+            <Stack.Screen name="Leaderboard" component={LeaderBoardScreen} />
 
-            <Stack.Screen name="Issue Details" component={IssueDetailScreen} />\
+            <Stack.Screen name="Issue Details" component={IssueDetailScreen} />
             <Stack.Screen name="Error" component={ErrorScreen} />
         </Stack.Navigator>
     );
