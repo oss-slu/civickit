d772d3d07e4a92bc9ae6a0387ff27da5c3157115
LeahBragg <leahb2613@gmail.com>
2026-05-26 09:02:42 -0500

    profile edits

--- mobile/src/screens/Profile/ProfileScreen.tsx
+++ mobile/src/screens/Profile/ProfileScreen.tsx
@@ -3,14 +3,14 @@ import IconButton from "../../components/IconButton";
 import { MessageView } from "../../components/MessageView";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native"
 import { useContext, useRef, useState } from "react";
-import { palette, colors, globalStyles, size, spacing, typography } from "../../styles";
+import { palette, colors, globalStyles, size, spacing, typography, borderRadius } from "../../styles";
 import { useAuth } from "../../contexts/AuthContext";
-import { FullWindowOverlay } from "react-native-screens";
 import { EditIcon, SettingsIcon, TrashIcon, UserIcon } from "../../components/Icons";
 import Button from "../../components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { StackParams } from "../../types/StackParams";
 import { StackNavigationProp } from "@react-navigation/stack";
+import { Image } from "expo-image";
 
 export default function ProfileScreen() {
     const { logout } = useAuth();
@@ -19,26 +19,40 @@ export default function ProfileScreen() {
 
     const navigation = useNavigation<StackNavigationProp<StackParams>>();
 
+    if (user == null) {
+        return (
+            <View style={styles.container}>
+                <Text style={[globalStyles.heading1]}>You are not logged in.</Text>
+                <Button text="Go to login" onPress={logout} />
+            </View>
+        )
+    }
     return (
         <View style={[styles.container]}>
-            {user?.profileImage != null ?
-                <UserIcon /> :
-                <TrashIcon />}
-            {/* Replace trash can with User image once implemented */}
-            <IconButton
-                onPress={() => navigation.navigate("Avatar", {})}>
-                <EditIcon />
-            </IconButton>
-
-            <Text style={[styles.message]}>{user?.name}</Text>
-
-            <IconButton style={{ flexDirection: "row", columnGap: spacing.sm }}
+            <IconButton style={{ ...styles.button, flexDirection: "row", columnGap: spacing.sm, alignSelf: "flex-end" }}
                 onPress={() => navigation.navigate("Settings", {})}>
-                <SettingsIcon />
-                <Text>Settings</Text>
+                <SettingsIcon color={styles.button.color} size={styles.button.fontSize} />
+                <Text style={{ color: styles.button.color, fontSize: styles.button.fontSize }}>Settings</Text>
             </IconButton>
 
-            <Button text="Logout" onPress={logout} />
+            <View style={styles.profilePicContainer}>
+                {user?.profileImage != null ?
+                    <Image
+                        source={require("../../../assets/ProfilePlaceholder.png")}
+                        style={styles.profilePic} /> :
+                    <TrashIcon />}
+                {/* Replace trashcan with User image once implemented */}
+                <View style={styles.editButton}>
+                    <IconButton
+                        onPress={() => navigation.navigate("Avatar", {})}>
+                        <EditIcon />
+                    </IconButton>
+                </View>
+            </View>
+
+            <Text style={globalStyles.heading1}>{user?.name}</Text>
+
+            <Button text="Logout" onPress={logout} style={styles.button} />
 
 
         </View>
@@ -47,27 +61,26 @@ export default function ProfileScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        width: '100%',
-        height: 500,
-        justifyContent: 'center',
+        ...globalStyles.container,
         alignItems: 'center',
+        rowGap: spacing.md
     },
-    message: {
-        fontSize: 20,
-        fontWeight: 500,
-        marginBottom: 15
+    button: {
+        ...globalStyles.button,
+        backgroundColor: palette.ckMediumGray,
+        fontSize: typography.sizeLg,
     },
-    logoutButton: {
-        alignContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.success,
-        paddingVertical: spacing.sm,
-        width: 150,
-        borderRadius: 10,
+    profilePicContainer: {
+
     },
-    logoutText: {
-        fontSize: typography.sizeSm,
-        color: palette.ckLight,
-        fontWeight: typography.weightBold,
+    profilePic: {
+        width: 200,
+        height: 200,
+        borderRadius: borderRadius.full,
+    },
+    editButton: {
+        position: "absolute",
+        bottom: 10,
+        right: 10
     }
 })
