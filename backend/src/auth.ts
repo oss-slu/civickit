1ffa0d8b71a82f5344c3cd642b7e66ee4e7cb7ef
LeahBragg <leahb2613@gmail.com>
2026-06-30 12:41:56 -0500

    auth set up

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -11,7 +11,8 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true
-    }
+    },
+    trustedOrigins: ["http://localhost:3001"],
 });
 
 import { betterAuth } from "better-auth";
