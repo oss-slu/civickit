c31c74ac80c50761a532614fcbe8f65a366ca226
bhuelsman <briana.huelsman@gmail.com>
2026-07-11 14:14:17 -0500

    removed unecessary imports

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,8 +1,5 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import prisma from "./prisma";
 
 export const auth = betterAuth({
