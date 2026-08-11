592bec6c792dd4e13a155f1a4e4f585d1d8b9b52
LeahBragg <leahb2613@gmail.com>
2026-08-11 10:36:06 -0500

    added this address doesn't look right option

---
+++ mobile/src/components/MiniMap.tsx
@@ -0,0 +1,40 @@
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import Pin from "./Pin";
+import { StyleSheet, View } from "react-native"
+import { borderRadius } from "../styles";
+
+export default function MiniMap({ issue, style, draggable = null, onMarkerDragEnd, ref }: any) {
+    return (
+        <MapView
+            style={{ ...styles.map, ...style }}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+                latitude: issue.latitude,
+                longitude: issue.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            }}
+            ref={ref}
+        >
+            <Marker
+                coordinate={{
+                    latitude: issue.latitude,
+                    longitude: issue.longitude,
+                }}
+                draggable
+                onDragEnd={(e) => onMarkerDragEnd(e.nativeEvent.coordinate)}
+            >
+
+                <Pin issue={issue} />
+            </Marker>
+        </MapView>
+    )
+}
+
+const styles = StyleSheet.create({
+    map: {
+        height: 220,
+        borderRadius: borderRadius.lg,
+        overflow: 'hidden',
+    },
+})
